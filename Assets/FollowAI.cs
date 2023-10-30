4d06589c46d0caf7c375b0273c68902f9b70d8aa
ccastro1016 <andycastro.001016@gmail.com>
2023-10-30 09:19:38 -0500

    Cambios pendientes

--- Assets/FollowAI.cs
+++ Assets/FollowAI.cs
@@ -2,29 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FollowAI : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Transform player;
 
-    private bool isFacingRight = true;
+    private Rigidbody2D rb2d;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.fixedDeltaTime);
+        // Calcula la dirección hacia la que el monstruo debe moverse.
+        moveDirection = (player.position - transform.position).normalized;
 
-        bool isPlayerRight = transform.position.x < player.position.x;
-        Flip(isPlayerRight);
-    }
-
-    private void Flip(bool isPlayerRight)
-    {
-        if((isFacingRight && !isPlayerRight) || (!isFacingRight && isPlayerRight))
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 scale = transform.localScale;
-            scale.x *= -1;
-            transform.localScale = scale;
-        }
+        // Mueve el monstruo utilizando el Rigidbody2D y asegúrate de que choque con objetos 2D.
+        rb2d.MovePosition(rb2d.position + moveDirection * speed * Time.fixedDeltaTime);
     }
 }
